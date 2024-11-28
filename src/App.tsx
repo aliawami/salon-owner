2a5783261762ec99caf14ead5b60d7fd3323f49c
Ali Alawami <alis2012@gmail.com>
2024-11-28 21:14:18 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+import Home from "./home_page/Home";
+
+function App() {
+  return (
+    <div className="mt-5" data-bs-theme="dark">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
